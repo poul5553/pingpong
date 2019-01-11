dff84392f1429e65efb7cb581efd27681a2b45bf
Koyangidees <jdh89419@hotmail.com>
2019-01-11 09:05:19 +0100

    edit field size

--- public_html/index.php
+++ public_html/index.php
@@ -1,5 +1,48 @@
 <?php include("assets/include/header.php"); ?>
 
 <h1>Index.php her</h1>
-<h2></h2>
+
 <?php include("assets/include/footer.php"); ?>
+
+
+    
+    <section class="board">
+ 
+        <h1 class="myH1">PINGPONG</h1>
+        
+        <div id="playSetting">
+            <button class="how" id="how">HOW TO PLAY</button>
+            <button class="setting" id="setting">SETTINGS</button>
+        </div>
+        
+        <div id="gameRecord">
+            <div id="player1">
+                <text class="score" id="playerOne" x="200" y="50">Player 1</text>
+                <text class="score" id="playerScore" x="200" y="50">0</text>
+            </div>
+            <div id="startReset">
+                <button class="start" id="start">START</button>
+                <button class="reset" id="reset">RESET</button>
+            </div>
+            <div id="player2">
+                <text class="score" id="playerTwo-score" x="200" y="50">0</text>
+                <text class="score" id="playerTwo" x="200" y="50">Player 2</text>
+
+            </div>
+        </div>
+
+   
+    
+        <div id="field">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 800 600" preserveAspectRatio="xMidYMid">
+                <rect id="background" width="800" height="600"></rect>
+
+                <rect id="paddleLeft" width="20" height="100" x="10" y="200"></rect>
+                <rect id="paddleRight" width="20" height="100" x="770" y="200"></rect>
+
+                <circle id="ball" r="10" cx="400" cy="250"></circle>
+            </svg>
+        </div>
+        
+    </section>
+    
